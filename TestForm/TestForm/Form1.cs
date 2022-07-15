2dd73de6ba42edf7734c6b59a9a2cff8c5a52b55
DeepakSureshTS <48537493+DeepakSureshTS@users.noreply.github.com>
2022-07-15 15:36:04 +0530

    New commit for button

---
+++ TestForm/TestForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username, password;
+            username = txtUser.Text;
+            password = txtPass.Text;
+        if (username=="admin" && password=="admin")
+            {
+                MessageBox.Show("Suceessfull login");
+            }
+        else
+            {
+                MessageBox.Show("error");
+            }
+        }
+    }
+}
